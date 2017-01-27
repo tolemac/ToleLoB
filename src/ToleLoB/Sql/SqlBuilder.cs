0ff258edd264b58dce19f9c88a1d4c5e129ad7c5
Javier Ros <tolemac@jros.org>
2017-01-27 22:47:50 +0100

    wip: remove unused usings

--- src/ToleLoB/Sql/SqlBuilder.cs
+++ src/ToleLoB/Sql/SqlBuilder.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using ToleLoB.Sql.Exceptions;
 using ToleLoB.Sql.Where;
 using ToleLoB.Sql.Table;
--- src/ToleLoB/Sql/Table/SqlJoin.cs
+++ src/ToleLoB/Sql/Table/SqlJoin.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq.Expressions;
 using ToleLoB.Sql.Where;
 
 namespace ToleLoB.Sql.Table
